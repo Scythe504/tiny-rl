a193b4b809bd45cbb81638faaa11facc114d18cb
Scythe504 <hellfireprince98@gmail.com>
2025-10-09 17:07:07 +0530

    fix: blocked original-referrer header not allowed

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -57,7 +57,7 @@ func (s *Server) corsMiddleware(next http.Handler) http.Handler {
 		// CORS Headers
 		w.Header().Set("Access-Control-Allow-Origin", "*") // Wildcard allows all origins
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-Original-Referrer")
 		w.Header().Set("Access-Control-Allow-Credentials", "false") // Credentials not allowed with wildcard origins
 
 		// Handle preflight OPTIONS requests
